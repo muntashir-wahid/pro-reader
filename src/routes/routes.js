1e8ac9d5574e72f8eb7f7d06e43804cef46771d9
muntashir-wahid <muntashir.wahid@yahoo.com>
2022-10-08 21:33:05 +0600

    completed router functionility for this porject

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import About from "../components/About";
+import BookDetails from "../components/BookDetails";
 import Books from "../components/Books";
 import ErrorPage from "../components/ErrorPage";
 import Home from "../components/Home";
@@ -14,7 +15,17 @@ export const router = createBrowserRouter([
       { index: true, element: <Home /> },
       { path: "home", element: <Home /> },
       { path: "about", element: <About /> },
-      { path: "books", element: <Books /> },
+      {
+        path: "books",
+        element: <Books />,
+        loader: async () => fetch("https://api.itbook.store/1.0/new"),
+      },
+      {
+        path: "book/:id",
+        element: <BookDetails />,
+        loader: async ({ params }) =>
+          fetch(`https://api.itbook.store/1.0/books/${params.id}`),
+      },
     ],
   },
 ]);
